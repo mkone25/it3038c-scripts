394ad4ad7cb2b0804b1edb7aa5ff1dbdcb77696e
mkone25 <konemuc@gmail.com>
2021-01-31 18:56:39 -0500

    Modified

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -1,6 +1,14 @@
-﻿function getIP{    
+﻿#$Hello = "Hello Powershell!"
+#Write-Host($Hello)
+function getIP{    
 (get-netipaddress).ipv4address | Select-String "192*"}
+
 write-host(getIP)
+$Version = $Host.Version.Major
 $IP = getIP
-Write-Host(“This machine’s IP is $IP”)
-Write-Host(“This machine’s IP is {0}” -f $IP)
+$Date = Get-Date
+$Body = "This machine’s IP is $IP. User is $env:username. Hostname is $env:computername.
+Powershell version $Version. Today's date and time is $Date "
+
+Write-Host($Body)
+#Send-MailMessage -To "konemuc@gmail.com" -From "konemuc@gmail.com" -Subject "IT3038C Window's sysinfo" -Body $Body -SmtpServer smtp.google.com -port 587 -UseSSL -Credential (Get-Credential)
