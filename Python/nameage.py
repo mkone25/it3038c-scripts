d8a2b4196d2ed8c1c636ba610dc7ac825867c675
mkone25 <konemuc@gmail.com>
2021-02-11 12:03:06 -0500

    added a new directory and file

---
+++ Python/nameage.py
@@ -0,0 +1,8 @@
+#!/python
+
+print('What is your name?')
+myName = input()
+print('Hello,'+ myName+'.That is a good name. How old are you?')
+myAge = input()
+print(myAge+'? That is funny,I am only a few seconds old.')
+print("I wish I was"+myAge*2+"year old" )
